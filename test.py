5e6184640c4c8643aaea0d24bc9b0573cabf0d00
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-04-19 18:36:53 -0400

    Code things

--- test.py
+++ test.py
@@ -5,9 +5,8 @@ url = 'http://192.168.1.49:5000/'
 def Get_Image():
 	try:
 		data = requests.get(url)
-		f = open('image.png', 'wb')
-		f.write(data.content)
-		f.close()
+		with open('image.png', 'wb') as f:
+			f.write(data.content)
 	except Exception as e:
 		print(e)
 
