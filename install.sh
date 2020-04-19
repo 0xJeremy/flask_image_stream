5b36bfb7c042bc22eff7927daeea261486e3215f
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-04-19 18:24:50 -0400

    added things

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/0xJeremy/flask_image_stream.git
 cd flask_image_stream
 echo "FLASK_APP=/home/pi/flask_image_stream/app.py" >> ~/.bashrc
-echo "flask run" >> ~/.bashrc
+echo "flask run --host=0.0.0.0" >> ~/.bashrc
 source ~/.bashrc
 echo "Done installing."
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:5000/'
+url = 'http://192.168.1.49:5000/'
 
 def Get_Image():
 	try:
